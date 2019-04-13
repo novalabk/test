72b10dca2e34e42b872400a802d87c6691727170
novalabk <khkim@novalabk.com>
2019-04-13 19:04:39 +0900

    init

---
+++ BootStudy20190413/src/main/java/com/apress/spring/BootStudy20190413Application.java
@@ -0,0 +1,13 @@
+package com.apress.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStudy20190413Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStudy20190413Application.class, args);
+	}
+
+}
